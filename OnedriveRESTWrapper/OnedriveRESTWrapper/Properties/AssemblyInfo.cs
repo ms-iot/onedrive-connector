a1a04a934b85f822980d10abe68896b079daf7ca
Matt McKee <mamcke@microsoft.com>
2015-11-02 14:37:24 -0800

    Update AssemblyInfo.cs

--- OnedriveRESTWrapper/OnedriveRESTWrapper/Properties/AssemblyInfo.cs
+++ OnedriveRESTWrapper/OnedriveRESTWrapper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OnedriveRESTWrapper")]
+[assembly: AssemblyTitle("OneDriveConnector")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("OnedriveRESTWrapper")]
+[assembly: AssemblyProduct("OneDriveConnector")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
