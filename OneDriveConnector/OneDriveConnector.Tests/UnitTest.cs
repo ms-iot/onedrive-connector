0b560a08f3b5f39023f6b141f9895182371c1450
Matthew McKee <mamcke@microsoft.com>
2015-12-14 15:29:48 -0800

    start test for login

--- OneDriveConnector/OneDriveConnector.Tests/UnitTest.cs
+++ OneDriveConnector/OneDriveConnector.Tests/UnitTest.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Microsoft.Maker.Storage;
+
 
 namespace OneDriveConnector.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class OneDriveConnectorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public async void TestLoginFailsAsExpected()
         {
+            // arange
+            var oneDriveConnector = new Microsoft.Maker.Storage.OneDrive.OneDriveConnector();
+
+            //act
+            try
+            {
+                await oneDriveConnector.LoginAsync("noClientID", "noClientSecret", "noRedirectUrl", "noAccessCode");
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            //assert
         }
     }
 }
